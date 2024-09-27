d95ce1b1e558066883f9bf6f35c5150453b6e1b4
danmlarsen <larsendanmarius@gmail.com>
2024-09-27 10:26:03 +0200

    Minor adjustments

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,3 @@
-import { ReactNode } from 'react';
-
-export default function Main({ children }: { children: ReactNode }) {
-    return <main className="min-h-screen md:min-h-[512px] flex flex-col bg-white md:min-w-[736px] md:flex-row md:rounded-4xl md:overflow-hidden md:shadow-3xl">{children}</main>;
+export default function Main({ children }: { children: React.ReactNode }) {
+    return <main className="min-h-screen md:min-h-[32rem] flex flex-col bg-white w-full md:max-w-[46rem] md:flex-row md:rounded-4xl md:overflow-hidden md:shadow-3xl">{children}</main>;
 }
--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -5,9 +5,9 @@ import { Score } from './App';
 
 export default function Summary({ scores }: { scores: Score[] }) {
     return (
-        <div className="p-8 flex flex-col flex-1 text-dark-grey-blue md:px-10 md:py-9">
+        <div className="px-8 py-6 flex flex-col flex-1 text-dark-grey-blue md:px-10 md:py-9">
             <h3 className="mb-6 text-lg font-bold md:text-2xl md:mb-7">Summary</h3>
-            <ul className="space-y-4 mb-10">
+            <ul className="space-y-4 mb-6 md:mb-10">
                 {scores.map(item => (
                     <SummaryItem {...item} />
                 ))}
